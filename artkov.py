2043c981004ad8576d2d0d8506030a8ce0a815b3
Danny Little <dannyl9979@gmail.com>
2021-02-26 14:50:02 -0500

    added docstrings and more explanatory comments

--- artkov.py
+++ artkov.py
@@ -34,6 +34,7 @@ DECREASE_REPEATS_PARAM = 0.4
 # closer to 1 => repeats are the most likely choice
 # closer to 0 => repeats will never occur
 
+# Make the transition matrix for dot size (radius)
 size_tmatrix = [[] for _ in range(MAX_DOT_SIZE + 1)]
 for i in range(len(size_tmatrix)):
     row = size_tmatrix[i]
@@ -45,26 +46,42 @@ for i in range(len(size_tmatrix)):
         else:
             row.append(SIZE_DROPOFF ** abs(j - i))
 
+# Make the transition matrix for dot color
 color_tmatrix = [[] for _ in range(255 + 1)]
-
 for i in range(len(color_tmatrix)):
     row = color_tmatrix[i]
     for j in range(len(color_tmatrix)):
         row.append(COLOR_DROPOFF ** abs(j - i))
 
 
-def rgbToHex(rgb):
+def rgbToHex(rgb) -> str:
+    """Returns a hex code given an rgb color value.
+        Args:
+            rgb (list): the red, green, and blue color values
+    """
     r, b, g = [int(max(0, min(255, x))) for x in rgb]
     return "#{0:02x}{1:02x}{2:02x}".format(r, b, g)
 
 
-def hexToRgb(hexcode):
+def hexToRgb(hexcode) -> tuple:
+    """Returns a tuple of rgb values given a hex code.
+        Args:
+            hexcode (str): the hex code to convert to rgb.
+    """
     hexcode = hexcode[1:]
     return tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4))
 
 
 class Dot():
     def __init__(self, canvas, center, radius, fill, **kwargs):
+        """
+        Represents a dot on a canvas. Automatically adds itself to its canvas' list of dots.
+            Args:
+            canvas (MyCanvas | InitialCanvas): the parent canvas of this dot.
+            center (list): the x, y value for the center of the dot.
+            radius (int): the radius (px) of the dot.
+            fill (str): the fill color of the dot. (hex code)
+        """
         self.canvas = canvas
         self.radius = int(radius)
         self.center = center
@@ -80,10 +97,21 @@ class Dot():
 
 class MyCanvas(Canvas):
     def __init__(self, parent, **kwargs):
+        """
+        A class representing a tkinter canvas object. 
+        Args:
+            parent (tkinter.Frame): the parent frame for this canvas.
+        """
         super().__init__(parent, **kwargs)
         self.dots = []
 
     def copy_canvas(self, last_canvas):
+        """
+        Deletes all of this canvas's dots and replaces them 
+        with duplicates of last_canvas's dots. 
+            Args:
+                last_canvas (MyCanvas): the canvas to copy dot size/center/color from.
+        """
         if self == last_canvas:
             return
         self.delete("all")
@@ -91,7 +119,12 @@ class MyCanvas(Canvas):
         for dot in last_canvas.dots:
             Dot(self, dot.center, dot.radius, dot.fill)
 
-    def makeNext(self, last_canvas):
+    def make_new_art(self, last_canvas):
+        """Removes dot on the current canvas, and makes a new art piece
+        based on the dots from last_canvas, using a markov chain to change dot size and color.
+            Args:
+                last_canvas (MyCanvas): the canvas to use as the initial state for the markov chain.
+        """
         inspiration_dots = last_canvas.dots
         self.delete("all")
         self.dots = []
@@ -116,25 +149,46 @@ class MyCanvas(Canvas):
 
 class InitialCanvas(MyCanvas):
     def __init__(self, parent, **kwargs):
+        """
+        A class representing a tkinter canvas, with more specific methods
+        for the initial canvas. This canvas can be clicked on to create new dots.
+        """
         super().__init__(parent, **kwargs)
-        self.bind("<Button-1>", self.handleClick)
+        self.bind("<Button-1>", self.handle_click)
         self.dotFill = "#000000"
         self.dotSize = INIT_DOT_SIZE
 
-    def handleClick(self, e):
+    def handle_click(self, e):
+        """Makes a new dot where the user clicked.
+            Args:
+                e (<Button-1> event): has x and y location of mouse click.
+        """
         center = [e.x, e.y]
         Dot(self, center, self.dotSize, self.dotFill)
 
     def pickColor(self):
+        """
+        Updates the InitialCanvas' dotFill attribute when a new color is picked.
+        """
         color_code = colorchooser.askcolor(title="Choose color")
         self.dotFill = color_code[1]
 
     def setDotSize(self, val):
+        """
+        Updates the InitialCanvas' dotSize attribute when a new radius is picked.
+        """
         self.dotSize = int(val)
 
 
 class CanvasGrid():
     def __init__(self, center_frame, initial_dots):
+        """
+        Creates 9 instances of MyCanvas. These are used after the initial art piece creation.
+            Args:
+            center_frame (tkinter.Frame): the parent frame for the canvases. 
+            initial_dots (list): A list of dots that the InitialCanvas had, to be copied into
+            the middle canvas of the grid.
+        """
         self.canvases = []
         for i in range(9):
             row = floor(i / 9 * 3)
@@ -152,21 +206,34 @@ class CanvasGrid():
 
         for i in range(9):
             if i != 4:
-                self.canvases[i].makeNext(self.middle)
+                self.canvases[i].make_new_art(self.middle)
 
         for i, canvas in enumerate(self.canvases):
             canvas.bind("<Button-1>", self.handle_click(i))
 
     def handle_click(self, last_canvas_idx):
+        """Replicates the dots from the clicked canvas on the middle canvas,
+        and generates new art in the other 8 canvases.
+            Args: last_canvas_idx: the index of the last canvas in this CanvasGrid's 
+            list of canvases.
+
+        Returns a second order function to bind to the <Button-1> event. 
+        """
         def func(*args):
             self.canvases[4].copy_canvas(self.canvases[last_canvas_idx])
             for i, canvas in enumerate(self.canvases):
                 if i != 4:
-                    canvas.makeNext(self.middle)
+                    canvas.make_new_art(self.middle)
         return func
 
 
-def create_grid_frame(frame, center_options, init_canvas):
+def create_grid_frame(frame, init_canvas):
+    """
+    Creates and places a grid of 9 canvases, as well as some controls, on the window.
+        Args:
+            frame (tkinter.Frame): the parent frame for the canvas grid and controls.
+            init_canvas (InitialCanvas): the canvas to base the artwork on initially.
+    """
     for widget in center_frame.winfo_children():
         widget.destroy()
 
@@ -189,6 +256,12 @@ def create_grid_frame(frame, center_options, init_canvas):
 
 
 def create_initial_frame(center_frame):
+    """
+    Creates and places a canvas, as well as some controls, on the window.
+    The user can click on the canvas to make dots. 
+        Args:
+            center_frame (tkinter.Frame): the parent frame for the initial canvas and controls.
+    """
     center_options = Frame(center_frame)
     init_canvas = InitialCanvas(
         center_frame, bg="white", height=INIT_CANVAS_SIZE, width=INIT_CANVAS_SIZE)
@@ -202,7 +275,7 @@ def create_initial_frame(center_frame):
 
     # go_button = Button(center_options, text="Make some random art!", command=lambda: artstart(center_frame, init_canvas))
     go_button = Button(center_options, text="Make some random art!",
-                       command=lambda: create_grid_frame(center_frame, center_options, init_canvas))
+                       command=lambda: create_grid_frame(center_frame, init_canvas))
 
     init_canvas.grid(row=0, column=0)
     center_options.grid(row=0, column=1)
